ea07cc5768b5e35e6ec2fd6902708d004c41c167
MischaGithub <mischa.achmat120212@gmail.com>
2020-07-07 00:21:04 -1000

    checking status

--- src/components/main.js
+++ src/components/main.js
@@ -7,7 +7,7 @@ import About from "./aboutme";
 import Contact from "./contactme";
 const Main = () => (
   <Switch>
-    <Route exact path="/Morgan-Portfolio" component={Portfolio} />
+    <Route exact path="/Morgan-Portfolio/portfolio" component={Portfolio} />
     <Route path="/Morgan-Portfolio/collection" component={Collection} />
     <Route path="/Morgan-Portfolio/aboutme" component={About} />
     <Route path="/Morgan-Portfolio/contactme" component={Contact} />
