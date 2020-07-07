47408557ffa4799fe432471e5938651c476d61b4
MischaGithub <mischa.achmat120212@gmail.com>
2020-07-07 00:13:48 -1000

    Updates

---
+++ precache-manifest.e95d60257daec68541776ec41ef1b34f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ae9493b75d487a216a587cdcfabbf94",
+    "url": "/MischaGithub/morganportolio.io/Morgan-Portfolio/index.html"
+  },
+  {
+    "revision": "35dfa6ada111c0ba8b59",
+    "url": "/MischaGithub/morganportolio.io/Morgan-Portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "35c56d6e4e9154b8c5ce",
+    "url": "/MischaGithub/morganportolio.io/Morgan-Portfolio/static/css/main.71745e1c.chunk.css"
+  },
+  {
+    "revision": "35dfa6ada111c0ba8b59",
+    "url": "/MischaGithub/morganportolio.io/Morgan-Portfolio/static/js/2.f684952a.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/MischaGithub/morganportolio.io/Morgan-Portfolio/static/js/2.f684952a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35c56d6e4e9154b8c5ce",
+    "url": "/MischaGithub/morganportolio.io/Morgan-Portfolio/static/js/main.ae644bbf.chunk.js"
+  },
+  {
+    "revision": "65d6330db046ff6252e0",
+    "url": "/MischaGithub/morganportolio.io/Morgan-Portfolio/static/js/runtime-main.8bde4ab0.js"
+  }
+]);
